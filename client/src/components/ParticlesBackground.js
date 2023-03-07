6e865cc66856037e2fc2793887fdfede6696bcb0
Fr0stb1t3 <monishx100@gmai.com>
2023-03-08 02:29:15 +0530

    Fixed Hexagon Issue

--- client/src/components/ParticlesBackground.js
+++ client/src/components/ParticlesBackground.js
@@ -2,28 +2,31 @@ import Particles from 'react-tsparticles'
 import { loadFull } from "tsparticles"
 import React, { useCallback } from "react"
 import particlesConfig from './config/particles-config'
-const ParticlesBackground = () =>{
-    const particlesInit = useCallback(async (engine)=>{
-        await loadFull(engine);
-    },[]);
-    const particlesLoaded = useCallback(async (container)=>{
+import { tsParticles } from "tsparticles-engine";
+import { loadPolygonPath } from "tsparticles-path-polygon";
 
-    },[]);
-    return(
-        <Particles 
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        style={{
-            width: "100%",
-            height: "100%",
-            position: "absolute",
-            top: "0",
-            margin: "0",
-            padding: "0",
-            left: "0"
-        }}
-        params={particlesConfig}/>
-    )
+loadPolygonPath(tsParticles);
+function ParticlesBackground() {
+    const particlesInit = useCallback(async (engine) => {
+        await loadFull(engine);
+    }, []);
+    const particlesLoaded = useCallback(async (container) => {
+    }, []);
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                top: "0",
+                margin: "0",
+                padding: "0",
+                left: "0"
+            }}
+            params={particlesConfig} />
+    );
 }
 export default ParticlesBackground
